773790ec6d2819ea30eeacf9e7b2bbd73c9ec437
vinhhn3 <vinhhn3@fe.edu.vn>
2026-03-15 08:24:55 +0700

    feat: implement authentication and authorization features with login, profile, and admin views

--- week-9-10-11/src/router/index.js
+++ week-9-10-11/src/router/index.js
@@ -1,13 +1,19 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import AboutView from "../views/AboutView.vue";
+import AdminView from "../views/AdminView.vue";
 import HomeView from "../views/HomeView.vue";
+import LoginView from "../views/LoginView.vue";
 import ProductView from "../views/ProductView.vue";
+import ProfileView from "../views/ProfileView.vue";
 
 const routes = [
   { path: "/", component: HomeView },
   { path: "/about", component: AboutView },
   { path: "/products", component: ProductView },
+  { path: "/profile", component: ProfileView },
+  { path: "/login", component: LoginView },
+  { path: "/admin", component: AdminView },
 ];
 
 const router = createRouter({
